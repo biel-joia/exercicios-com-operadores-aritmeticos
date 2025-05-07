0e84af6852c9a347312daef70f97dc1bd4d84a62
biel-joia <g.joia@aluno.ifsp.edu.br>
2025-05-07 18:31:22 -0300

    Add files via upload

---
+++ exercicio 6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            // Fórmula da média geométrica: raiz quadrada de (valor1 * valor2)
+            double mediaGeometrica = Math.Sqrt(valor1 * valor2);
+
+            Console.WriteLine("A média geométrica é: " + mediaGeometrica);
+        }
+    }
+}
