c639c40e17d109a9fb7bf1ea976ed5a52b924fb0
biel-joia <g.joia@aluno.ifsp.edu.br>
2025-05-07 18:30:36 -0300

    Add files via upload

---
+++ exercico 4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da base do triângulo: ");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura do triângulo: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double area = (baseTriangulo * altura) / 2;
+
+            Console.WriteLine("A área do triângulo é: ");
+        }
+    }
+}
