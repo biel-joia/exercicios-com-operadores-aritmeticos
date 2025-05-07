09cc4d043666d1763627a5a864fe5006b45a1306
biel-joia <g.joia@aluno.ifsp.edu.br>
2025-05-07 18:30:13 -0300

    Add files via upload

---
+++ exercicio 3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercício_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da diagonal do quadrado: ");
+            double diagonal = Convert.ToDouble(Console.ReadLine());
+
+            double area = (diagonal * diagonal) / 2;
+
+            Console.WriteLine("A área do quadrado é: ");
+        }
+    }
+}
