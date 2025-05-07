5fd64e35e0b6d3940b05ff8006998cd65f54782d
biel-joia <g.joia@aluno.ifsp.edu.br>
2025-05-07 18:31:46 -0300

    Add files via upload

---
+++ exercicio 7.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor em milhas marítimas: ");
+            double milhas = Convert.ToDouble(Console.ReadLine());
+
+            double quilometros = milhas * 1.852;
+
+            Console.WriteLine("O valor em quilômetros é: " + quilometros);
+        }
+    }
+}
