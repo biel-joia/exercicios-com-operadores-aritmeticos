5f690b8d79d78fe38a8c589f5f7f894e83d5f501
biel-joia <g.joia@aluno.ifsp.edu.br>
2025-05-07 18:33:04 -0300

    Add files via upload

---
+++ exercicio 9.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do raio do círculo: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            double area = Math.PI * raio * raio;
+
+            Console.WriteLine("A área do círculo é: " + area);
+        }
+    }
+}
